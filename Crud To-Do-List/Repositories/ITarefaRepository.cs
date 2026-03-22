23cd32943bcde40c7aaa41bb5d8fa8b04664b667
Rafael Santana <rafa03santana@gmail.com>
2026-03-22 20:36:45 -0300

    feat: cria interface ITarefaRepository

---
+++ Crud To-Do-List/Repositories/ITarefaRepository.cs
@@ -0,0 +1,11 @@
+﻿using Crud_To_Do_List.Models;   
+namespace Crud_To_Do_List.Repositories;
+
+public interface ITarefaRepository
+{
+    List<Tarefa> ListarTodas();
+    Tarefa? ObterPorId(int id);
+    void Criar(Tarefa tarefa);
+    void Atualizar(Tarefa tarefa);
+    void Deletar(Tarefa tarefa);
+}
