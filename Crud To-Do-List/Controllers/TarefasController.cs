fc8610102e6c8c46edbee6c4bef7c525c234373b
Rafael Santana <rafa03santana@gmail.com>
2026-03-22 20:37:36 -0300

    refactor: isola lógica de dados no repositório

--- Crud To-Do-List/Controllers/TarefasController.cs
+++ Crud To-Do-List/Controllers/TarefasController.cs
@@ -1,5 +1,6 @@
 ﻿using Crud_To_Do_List.Data;
 using Crud_To_Do_List.Models;
+using Crud_To_Do_List.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,23 +10,24 @@ namespace Crud_To_Do_List.Controllers;
 [ApiController]
 public class TarefasController : ControllerBase
 {
-    private readonly AppDbContext? _context;
+    private readonly ITarefaRepository _repository;
 
-    public TarefasController(AppDbContext? context)
+    // Agora o controller recebe o Repository, não mais o DbContext diretamente..
+    public TarefasController(ITarefaRepository repository)
     {
-        _context = context;
+        _repository = repository;
     }
 
     [HttpGet]
     public ActionResult<List<Tarefa>> ListarTarefas()
     {
-        return Ok(_context.Tarefas.ToList());
+        return Ok(_repository.ListarTodas());
     }
 
     [HttpGet("{id}")]
     public ActionResult<Tarefa> ObterTarefa(int id)
     {
-        var tarefa = _context.Tarefas.Find(id);
+        var tarefa = _repository.ObterPorId(id);
         if (tarefa == null)
             return NotFound();
         return Ok(tarefa);
@@ -38,8 +40,7 @@ public class TarefasController : ControllerBase
             return BadRequest("O título é obrigatório.");  
         
         novaTarefa.DataCriacao = DateTime.UtcNow;
-        _context.Tarefas.Add(novaTarefa);
-        _context.SaveChanges();
+        _repository.Criar(novaTarefa);
 
         return CreatedAtAction(nameof(ObterTarefa), new { id = novaTarefa.Id }, novaTarefa);
     }
@@ -47,14 +48,14 @@ public class TarefasController : ControllerBase
     [HttpPut("AtualizarTarefa/{id}")]
     public ActionResult AtualizarTarefa(int id, Tarefa tarefaAtualizada)
     {
-        var tarefa = _context.Tarefas.Find(id);
+        var tarefa = _repository.ObterPorId(id);
         if (tarefa == null)
             return NotFound();
 
         tarefa.Titulo = tarefaAtualizada.Titulo;
         tarefa.Descricao = tarefaAtualizada.Descricao;
         tarefa.Concluida = tarefaAtualizada.Concluida;
-        _context.SaveChanges();
+        _repository.Atualizar(tarefa);
 
         return NoContent();
     }
@@ -62,13 +63,11 @@ public class TarefasController : ControllerBase
     [HttpDelete("ExcluirTarefa/{id}")]
     public ActionResult ExcluirTarefa(int id)
     {
-        var tarefa = _context.Tarefas.Find(id); 
+        var tarefa = _repository.ObterPorId(id);
         if (tarefa == null)
             return NotFound();
 
-        _context.Tarefas.Remove(tarefa);
-        _context.SaveChanges();
-
+        _repository.Deletar(tarefa);
         return NoContent();
     }
 }
