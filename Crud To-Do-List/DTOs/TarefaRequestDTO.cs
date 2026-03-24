9460cdb7e7d9ab64e79956fbc404dbed4d73481d
Rafael Santana <rafa03santana@gmail.com>
2026-03-23 23:56:24 -0300

    feat(dto): adiciona DTOs para entrada e saída de tarefas

---
+++ Crud To-Do-List/DTOs/TarefaRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Crud_To_Do_List.DTOs;
+
+public class TarefaRequestDTO
+{
+    public string? Titulo { get; set; }
+    public string? Descricao { get; set; }
+    public bool Concluida { get; set; }
+}
---
+++ Crud To-Do-List/DTOs/TarefaResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Crud_To_Do_List.DTOs;
+
+public class TarefaResponseDto
+{
+    public int Id { get; set; }
+    public string? Titulo { get; set; } 
+    public string? Descricao { get; set; } 
+    public bool Concluida { get; set; }
+    public DateTime DataCriacao { get; set; }
+}
