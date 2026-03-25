3112fd998dc2cf96d3e1d51b995a9b202cdf446d
Rafael Santana <rafa03santana@gmail.com>
2026-03-24 22:46:44 -0300

    feat: Adiciona validacao com Data Annotations

--- Crud To-Do-List/DTOs/TarefaRequestDTO.cs
+++ Crud To-Do-List/DTOs/TarefaRequestDTO.cs
@@ -1,8 +1,15 @@
-﻿namespace Crud_To_Do_List.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud_To_Do_List.DTOs;
 
 public class TarefaRequestDTO
 {
+    [Required(ErrorMessage = "O título é obrigatório.")]
+    [MinLength(3, ErrorMessage = "O título deve conter pelo menos 3 caracteres.")]
+    [MaxLength(100, ErrorMessage = "O título deve conter no máximo 100 caracteres.")]
     public string? Titulo { get; set; }
+
+    [MaxLength(500, ErrorMessage = "A descrição deve conter no máximo 500 caracteres.")]
     public string? Descricao { get; set; }
     public bool Concluida { get; set; }
 }
